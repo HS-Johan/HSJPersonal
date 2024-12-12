112b31eb688a092f130220de36ffbffd8bbea857
Hasan Shahriar <johan.cse5.bu@gmail.com>
2024-12-12 14:07:45 +0600

    1st Default Code

---
+++ HSJPersonal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HSJPersonal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
